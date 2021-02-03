203035b50d45518b5ae46ddcd537271e51ba1d28
cs-moudgill <channpreet.moudgill@gmail.com>
2021-02-03 14:38:32 -0400

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCFnUF60R3NnNDVTg7J9irDy4HPph3Dg4E",
+  authDomain: "netflix-clone-8194c.firebaseapp.com",
+  projectId: "netflix-clone-8194c",
+  storageBucket: "netflix-clone-8194c.appspot.com",
+  messagingSenderId: "867901887046",
+  appId: "1:867901887046:web:de5a6bb5a6ab81df063047"
+};
